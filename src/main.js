3c69986f3002e4499a8da148652d8c7e770b4fbe
kent <kent.26pg@gmail.com>
2026-07-06 12:16:02 +0200

    init commit

---
+++ src/main.js
@@ -0,0 +1,15 @@
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import './style.css';
+
+var map = L.map('map').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    zoomLevel: 13,
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+L.marker([51.5, -0.09]).addTo(map)
+    .bindPopup('A pretty CSS popup.<br> Easily customizable.')
+    .openPopup();
