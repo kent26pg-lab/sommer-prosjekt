4633b146a3ef3e51a819a78915e29dcc956461ab
kent <kent.26pg@gmail.com>
2026-07-15 20:39:07 +0200

    added geocoding search with geoapify

--- src/main.js
+++ src/main.js
@@ -13,3 +13,23 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 L.marker([51.5, -0.09]).addTo(map)
     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
     .openPopup();
+
+const apiKey = import.meta.env.VITE_GEOAPIFY_API_KEY;
+
+async function searchLocation(query) {
+  const url = `https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(query)}&apiKey=${apiKey}`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if (data.features.length === 0) {
+    alert('Fant ikke stedet');
+    return;
+  }
+
+  const location = data.features[0];
+  const [lon, lat] = location.geometry.coordinates;
+
+  map.setView([lat, lon], 13);
+}
+
